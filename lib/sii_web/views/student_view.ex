1a93f9d2a910bc15d936b5e02f5ab0bf1d462fb4
Isidro Henoch <isidro.henoch@gmail.com>
2018-12-01 20:38:26 -0600

    Removes the password_hash from the student when reading data

--- lib/sii_web/views/student_view.ex
+++ lib/sii_web/views/student_view.ex
@@ -11,12 +11,13 @@ defmodule SiiWeb.StudentView do
   end
 
   def render("student.json", %{student: student}) do
-    %{id: student.id,
+    %{
+      id: student.id,
       first_name: student.first_name,
       last_name: student.last_name,
       email: student.email,
       control_number: student.control_number,
-      password_hash: student.password_hash,
-      image: student.image}
+      image: student.image
+    }
   end
 end
